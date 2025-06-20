0d72a28ca064e17d21599fa66e68b27751362a49
coderRitikPro <ritikorkumar@gmail.com>
2025-06-20 12:20:43 +0530

    pushed client

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,5 +1,5 @@
 const { createClient } = require('@supabase/supabase-js');
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
-// console.log(supabase, 'this is supabase client');
+console.log(supabase, 'this is supabase client',process.env.SUPABASE_URL);
 module.exports = supabase;
